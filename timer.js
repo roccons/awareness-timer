4f0ffd52dc0775835356dc8dd0775cc9504ebd2c
roccons <contacto@habitatweb.com.mx>
2015-04-02 20:44:31 -0600

    minutos y segundos, audio pausa

--- timer.js
+++ timer.js
@@ -1,51 +1,74 @@
 var how_many_minutes = 20;
 var how_many_seconds = 0;
-how_many_seconds += how_many_minutes*60;
 
 var remaining_seconds, myTimeout;
 var isCounting = false;
 
 // obtengo los tags
-var dom_countdown = document.getElementById("countdown");
+var countdown_s = document.getElementById("countdown_s");
 var dom_mysound = document.getElementById("mysound");
-var dom_startcount = document.getElementById("startcount");
-var dom_stopcount = document.getElementById("stopcount");
-var dom_testsound = document.getElementById("testsound");
+var btn_startcount = document.getElementById("startcount");
+var btn_stopcount = document.getElementById("stopcount");
+var btn_testsound = document.getElementById("testsound");
+var dom_totalminutes = document.getElementById("totalminutes");
 var dom_totalseconds = document.getElementById("totalseconds");
 var dom_totaltime = document.getElementById("totaltime");
 
 
-dom_countdown.innerHTML=how_many_seconds;
+countdown_s.innerHTML=how_many_seconds;
+dom_totalminutes.value=how_many_minutes;
 dom_totalseconds.value=how_many_seconds;
 
+function formato(valor) { // devuelve una cadena de dos dígitos
+	valor=""+valor;
+	if(valor.length<2) {
+		return "0" + valor;
+	} else {
+		return valor;
+	}
+}
+
+
 function startCountdown () {
+	remaining_minutes = dom_totalminutes.value;
 	remaining_seconds = dom_totalseconds.value;
 	document.title="▶ Activo";
 	dom_totaltime.style.display="none";
-	dom_countdown.style.display="inline-block";
-	dom_startcount.style.display="none";
-	dom_stopcount.style.display="block";
-	dom_countdown.innerHTML=dom_totalseconds.value;
+	countdown.style.display="inline-block";
+	btn_startcount.style.display="none";
+	btn_stopcount.style.display="block";
+	countdown_m.innerHTML=dom_totalminutes.value;
+	countdown_s.innerHTML=formato(dom_totalseconds.value);
 	myCountdown();
 }
 
 function stopCountdown () {
 	document.title="… Detenido";
 	dom_totaltime.style.display="inline-block";
-	dom_countdown.style.display="none";
-	dom_startcount.style.display="inline-block";
-	dom_stopcount.style.display="none";
+	countdown.style.display="none";
+	btn_startcount.style.display="inline-block";
+	btn_stopcount.style.display="none";
 	clearTimeout(myTimeout);
-	dom_countdown.innerHTML=dom_totalseconds.value;
+	dom_mysound.pause();
+	dom_mysound.currentTime=0;
 }
 
 function myCountdown () {
 	myTimeout = setTimeout(function (){
-		if (--remaining_seconds>0) {
-			dom_countdown.innerHTML=remaining_seconds;
-			document.title="▶ " + remaining_seconds;
+		if (remaining_seconds>0) {
+			remaining_seconds--;
+			countdown_s.innerHTML=formato(remaining_seconds);
+			document.title="▶ " + remaining_minutes + ":" + formato(remaining_seconds);
+			myCountdown();
+		} else if(remaining_minutes>0) {
+			remaining_minutes--;
+			remaining_seconds=59;
+			countdown_m.innerHTML=remaining_minutes;
+			countdown_s.innerHTML=formato(remaining_seconds);
+			document.title="▶ " + remaining_minutes + ":" + formato(remaining_seconds);
 			myCountdown();
 		} else {
+			dom_mysound.currentTime=0;
 			dom_mysound.play();
 			startCountdown();
 		}
@@ -54,6 +77,11 @@ function myCountdown () {
 
 startcount.onclick = startCountdown;
 stopcount.onclick = stopCountdown;
-dom_testsound.onclick = function () { 
-	dom_mysound.play();
+btn_testsound.onclick = function () { 
+	if(dom_mysound.currentTime>0 && dom_mysound.currentTime<30) {
+		dom_mysound.pause();
+		dom_mysound.currentTime=0;
+	} else {
+		dom_mysound.play();
+	}
 };
